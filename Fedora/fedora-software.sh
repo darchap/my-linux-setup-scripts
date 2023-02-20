cdcaf909c1cdbab22f04f2fb230cb229d1ba7ff8
darchap <113114289+darchap@users.noreply.github.com>
2023-02-20 20:45:11 +0100

    Added description comments on UFW section

--- Fedora/fedora-software.sh
+++ Fedora/fedora-software.sh
@@ -10,6 +10,11 @@ sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 sudo dnf install -y codium
 
+#VSCODE
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install -y code
+
 #BRAVE
 sudo dnf install dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
@@ -39,10 +44,10 @@ sudo dnf install -y git
 sudo dnf install -y zsh
 sudo chsh -s "$(which zsh)" "$USER"
 
-#starship
+#STARSHIP
 curl -sS https://starship.rs/install.sh | sh
 
-#nerd fonts
+#NERD FONTS
 git clone https://github.com/ryanoasis/nerd-fonts.git
 sudo ./nerd-fonts/install.sh -S Meslo
 
@@ -53,8 +58,8 @@ sudo systemctl disable --now firewalld.service
 #Configure ufw
 sudo ufw default deny incoming
 sudo ufw limit in 22/tcp comment "rate-limit SSH"
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
+sudo ufw allow 80/tcp comment 'accept HTTP connections'
+sudo ufw allow 443/tcp comment 'accept HTTPS connections'
 sudo ufw default allow outgoing
 sudo ufw enable
 
